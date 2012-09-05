cad8684e3ae23341a0d10e110ea093623b2cf135
Andrew Rea <email@andrewrea.co.uk>
2012-09-05 22:28:57 +0100

    Nearly at the end

--- iislogparser/parsers.py
+++ iislogparser/parsers.py
@@ -1,118 +1,196 @@
 from datetime import datetime
+import json
 import urlparse
+import linecache
 
-class W3CParser:
+class W3CFieldParser:
+
+    def parse(self, filename):
+        fieldsLine = linecache.getline(filename,4)
+        fields = fieldsLine[fieldsLine.index(": ")+2:]
+        return fields.split(" ")
+
+class W3CLogItemParser:
 
     def __init__(self, fields):
-        self.items = fields[fields.find(": ")+2:].split(" ")
+        self.items = fields[fields.find(": ")+2:].replace("\n","").split(" ")
         self.itemparsers = []
         for item in self.items:
             #Need to change below to have a helper function for the replace.  Avoiding regex due to performance
             #Only a certain number of chars to consider replacing at this stage.  Keeping with replace or helper function
-            self.itemparsers.append(lambda lineitems,output: \
+            self.itemparsers.append(lambda lineitems: \
                     getattr(self, "__add_"+item.replace("-","_").replace("(","").replace(")","").lower()+"__") \
-                    (lineitems,output,self.items.index(item)))
+                    (lineitems,self.items.index(item)))
 
     def parse(self, line):
         lineitems = line.split(" ")
         logitem = {}
         for itemparser in self.itemparsers:
-            itemparser(lineitems, logitem)
+            logitems = itemparser(lineitems)
+            logitem.update(logitems)
 
         return logitem
     
-    def __add_date__(self, lineitems, output, index):
+    def __add_date__(self, lineitems, index):
+        output = {}
         parsedDate = datetime.strptime(lineitems[index],"%Y-%m-%d")
         output["year"] = parsedDate.year
         output["month"] = parsedDate.month
         output["day"] = parsedDate.day
+        print("returning", output)
+        return output
     
-    def __add_time__(self, lineitems, output, index):
+    def __add_time__(self, lineitems, index):
+        output = {}
         parsedTime = datetime.strptime(lineitems[index],"%H:%M:%S")
         output["hour"] = parsedTime.hour
         output["minute"] = parsedTime.minute
         output["second"] = parsedTime.second
+        return output
         
-    def __add_c_ip__(self, lineitems, output, index):
+    def __add_c_ip__(self, lineitems, index):
+        output = {}
         output["c_ip"] = lineitems[index]
+        return output
 
-    def __add_cs_username__(self, lineitems, output, index):
+    def __add_cs_username__(self, lineitems, index):
+        output = {}
         output["cs_username"] = lineitems[index]
+        return output
 
-    def __add_s_sitename__(self, lineitems, output, index):
+    def __add_s_sitename__(self, lineitems, index):
+        output = {}
         output["s_sitename"] = lineitems[index]
+        return output
 
-    def __add_s_computername__(self, lineitems, output, index):
+    def __add_s_computername__(self, lineitems, index):
+        output = {}
         output["s_computername"] = lineitems[index]
+        return output
 
-    def __add_s_ip__(self, lineitems, output, index):
+    def __add_s_ip__(self, lineitems, index):
+        output = {}
         output["s_ip"] = lineitems[index]
+        return output
     
-    def __add_s_port__(self, lineitems, output, index):
+    def __add_s_port__(self, lineitems, index):
+        output = {}
         output["s_port"] = lineitems[index]
+        return output
 
-    def __add_cs_method__(self, lineitems, output, index):
+    def __add_cs_method__(self, lineitems, index):
+        output = {}
         output["cs_method"] = lineitems[index]
+        return output
 
-    def __add_cs_uri_stem__(self, lineitems, output, index):
+    def __add_cs_uri_stem__(self, lineitems, index):
+        output = {}
         output["cs_uri_stem"] = lineitems[index]
+        return output
 
-    def __add_cs_uri_query__(self, lineitems, output, index):
+    def __add_cs_uri_query__(self, lineitems, index):
+        output = {}
         querystring = lineitems[index]
         parsedQuerystring = urlparse.parse_qs(querystring)
         for key in parsedQuerystring.keys():
             if len(parsedQuerystring[key]) == 1:
                 parsedQuerystring[key] = parsedQuerystring[key][0]
         output["cs_uri_query"] = parsedQuerystring
+        return output
 
-    def __add_sc_status__(self, lineitems, output, index):
+    def __add_sc_status__(self, lineitems, index):
+        output = {}
         try:
             output["sc_status"] = int(lineitems[index])
         except ValueError:
             pass
+        return output
 
-    def __add_sc_sub_status__(self, lineitems, output, index):
+    def __add_sc_sub_status__(self, lineitems, index):
+        output = {}
         try:
             output["sc_sub_status"] = int(lineitems[index])
         except ValueError:
             #Not an int, apparantly in python they go with EAFP "Easier to ask for forgiveness than permission"
             pass
+        return output
 
-    def __add_sc_win32_status__(self, lineitems, output, index):
+    def __add_sc_win32_status__(self, lineitems, index):
+        output = {}
         try:
             output["sc_win32_status"] = int(lineitems[index])
         except ValueError:
             pass
+        return output
 
-    def __add_sc_bytes__(self, lineitems, output, index):
+    def __add_sc_bytes__(self, lineitems, index):
+        output = {}
         try:
             output["sc_bytes"] = int(lineitems[index])
         except ValueError:
             pass
+        return output
 
-    def __add_cs_bytes__(self, lineitems, output, index):
+    def __add_cs_bytes__(self, lineitems, index):
+        output = {}
         try:
             output["cs_bytes"] = int(lineitems[index])
         except ValueError:
             pass
+        return output
 
-    def __add_time_taken__(self, lineitems, output, index):
+    def __add_time_taken__(self, lineitems, index):
+        output = {}
         try:
-            output["time-taken"] = int(lineitems[index])
+            output["time_taken"] = int(lineitems[index])
         except ValueError:
             pass
+        return output
 
-    def __add_cs_version__(self, lineitems, output, index):
+    def __add_cs_version__(self, lineitems, index):
+        output = {}
         output["cs_version"] = lineitems[index]
+        return output
 
-    def __add_cs_host__(self, lineitems, output, index):
+    def __add_cs_host__(self, lineitems, index):
+        output = {}
         output["cs_host"] = lineitems[index]
+        return output
 
-    def __add_csuser_agent__(self, lineitems, output, index):
+    def __add_csuser_agent__(self, lineitems, index):
+        output = {}
         output["cs_user_agent"] = lineitems[index]
+        return output
 
-    def __add_cscookie__(self, lineitems, output, index):
+    def __add_cscookie__(self, lineitems, index):
+        output = {}
         output["cs_cookie"] = lineitems[index]
+        return output
 
-    def __add_csreferer__(self, lineitems, output, index):
+    def __add_csreferer__(self, lineitems, index):
+        output = {}
         output["cs_referer"] = lineitems[index]
+        return output
+
+class W3CIISLogJsonConverter:
+
+    def convert(self, infilename, outfilename):
+
+        linecount = 0
+        lines = ""
+        fieldsLine = linecache.getline(infilename,4)
+        logItemParser = W3CLogItemParser(fieldsLine)
+        encoder = json.JSONEncoder(ensure_ascii=False)
+        with open(infilename, "rb") as logfile:
+            with open(outfilename, "ab") as out:
+                for line in logfile:
+                    linecount += 1
+                    if linecount > 4:
+                        logitem = logItemParser.parse(line)
+                        jsonEncoded = encoder.encode(logitem)+"\n"
+                        lines += jsonEncoded
+                        if linecount % 25000 == 0:
+                            out.write(lines)
+                            lines = ""
+                out.write(lines)
+                        
--- iislogparser/tests/test-parsing-w3c.py
+++ iislogparser/tests/test-parsing-w3c.py
@@ -1,11 +1,18 @@
 import unittest
-from iislogparser.parsers import W3CParser
+from iislogparser.parsers import W3CLogItemParser
 
 class TestParsingW3CFormat(unittest.TestCase):
 
+    def testParsingFieldWithNewLine(self):
+        fields="#Fields: time-taken\n"
+        parser = W3CLogItemParser(fields)
+        line = "1234"
+        logItem = parser.parse(line)
+        self.assertEqual(logItem["time_taken"], 1234)
+
     def testParsingDate(self):
         fields = "#Fields: date"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "2012-07-30"
         logItem = parser.parse(line)
         self.assertEqual(logItem['year'], 2012)
@@ -14,7 +21,7 @@ class TestParsingW3CFormat(unittest.TestCase):
 
     def testParsingTime(self):
         fields = "#Fields: time"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "12:13:14"
         logItem = parser.parse(line)
         self.assertEqual(logItem['hour'], 12)
@@ -23,63 +30,63 @@ class TestParsingW3CFormat(unittest.TestCase):
 
     def testParsingClientIPAddress(self):
         fields = "#Fields: c-ip"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "10.6.111.6"
         logItem = parser.parse(line)
         self.assertEqual(logItem["c_ip"], line)
 
     def testParsingUserName(self):
         fields = "#Fields: cs-username"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "BOBO"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_username"], line)
 
     def testParsingSiteName(self):
         fields = "#Fields: s-sitename"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "somesitename"
         logItem = parser.parse(line)
         self.assertEqual(logItem["s_sitename"], line)
 
     def testParsingComputerName(self):
         fields = "#Fields: s-computername"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "COMP"
         logItem = parser.parse(line)
         self.assertEqual(logItem["s_computername"], line)
 
     def testParsingSIP(self):
         fields = "#Fields: s-ip"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "10.8.8.900"
         logItem = parser.parse(line)
         self.assertEqual(logItem["s_ip"], line)
 
     def testParsingSPort(self):
         fields = "#Fields: s-port"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "80"
         logItem = parser.parse(line)
         self.assertEqual(logItem["s_port"], line)
 
     def testParsingCsMethod(self):
         fields = "#Fields: cs-method"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "POST"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_method"], line)
 
     def testParsingCsUriStem(self):
         fields = "#Fields: cs-uri-stem"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "/a/b/c/d.aspx"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_uri_stem"], line)
 
     def testParsingCsUriQuery(self):
         fields = "#Fields: cs-uri-query"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "a=1&b=2"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_uri_query"]["a"],"1")
@@ -87,84 +94,92 @@ class TestParsingW3CFormat(unittest.TestCase):
 
     def testParsingScStatus(self):
         fields = "#Fields: sc-status"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "200"
         logItem = parser.parse(line)
         self.assertEqual(logItem["sc_status"], 200)
     
     def testParsingScSubStatus(self):
         fields = "#Fields: sc-sub-status"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "0"
         logItem = parser.parse(line)
         self.assertEqual(logItem["sc_sub_status"], 0)
 
     def testParsingNonIntSubStatus(self):
         fields = "#Fields: sc-sub-status"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "-"
         logItem = parser.parse(line)
         self.assertFalse("sc_sub_status" in logItem.keys())
 
     def testParsingScWin32Status(self):
         fields = "#Fields: sc-win32-status"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "0"
         logItem = parser.parse(line)
         self.assertEqual(logItem["sc_win32_status"], 0)
 
     def testParsingScBytes(self):
         fields = "#Fields: sc-bytes"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "3456"
         logItem = parser.parse(line)
         self.assertEqual(logItem["sc_bytes"], 3456)
 
     def testParsingCsBytes(self):
         fields = "#Fields: cs-bytes"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "1234"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_bytes"], 1234)
 
     def testParsingTimeTaken(self):
         fields = "#Fields: time-taken"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "234"
         logItem = parser.parse(line)
-        self.assertEqual(logItem["time-taken"], 234)
+        self.assertEqual(logItem["time_taken"], 234)
 
     def testParsingCsVersion(self):
         fields = "#Fields: cs-version"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "12"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_version"], "12")
 
     def testParsingCsHost(self):
         fields = "#Fields: cs-host"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "host"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_host"], line)
 
     def testParsingCsUserAgent(self):
         fields = "#Fields: cs(User-Agent)"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "blablaMozillaChromeEI"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_user_agent"],line) 
 
     def testParsingCsCookie(self):
         fields = "#Fields: cs(Cookie)"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "monster"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_cookie"], line)
 
     def testParsingCsReferer(self):
         fields = "#Fields: cs(Referer)"
-        parser = W3CParser(fields)
+        parser = W3CLogItemParser(fields)
         line = "http://www.google.com"
         logItem = parser.parse(line)
         self.assertEqual(logItem["cs_referer"], line)
+
+    def testParsingMultipleFields(self):
+        fields = "#Fields: cs-host time-taken"
+        parser = W3CLogItemParser(fields)
+        line = "host 1234"
+        logItem = parser.parse(line)
+        self.assertEqual(logItem["cs_host"], "host")
+        self.assertEqual(logItem["time_taken"], "1234")
